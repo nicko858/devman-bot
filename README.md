26f6dd1bfa02979d96f829e102ab885e554aac94
ikar2021@hotmail.com <ikar2021@hotmail.com>
2019-08-22 11:09:48 +0300

    - run_bot() with arguments - raise for http - https-proxy as env-variable - sleep if network error - edit readme

--- README.md
+++ README.md
@@ -22,13 +22,17 @@ $ pip install -r requirements.txt
 ### Telegram instructions
 You have to create telegram-channel, telegram-bot and get access-token.
 [This arcticle](https://smmplanner.com/blog/otlozhennyj-posting-v-telegram/) will helps you to do all of this things.
-If everything is fine, you'll get `telegram_bot_token`, `telegram_bot_url` and  `telegram_channel_name`.  
+If everything is fine, you'll get `telegram_bot_token`, `telegram_bot_url` and  `telegram_channel_name`.
+Next thing you should do is to get a proxy address (actual for Russian Federation).
+Check [spys.one](http://spys.one/proxys/US/) and get `https` proxy-address.
+  
 Add the following records to the `.env-file`:  
 
    ```bash
 TELEGRAM_BOT_TOKEN=Your bot token
 TELEGRAM_BOT_URL=Your bot address
 TELEGRAM_CHANNEL_NAME=Your telegram channel
+HTTPS_PROXY=<proxy address from spys.one>
   ```
 
 
--- devman_bot.py
+++ devman_bot.py
@@ -1,24 +1,38 @@
 import requests
 from requests import ConnectionError
 from requests import ReadTimeout
+from requests import HTTPError
 from dotenv import load_dotenv
 from os import environ
 from os import getenv
 import telegram
 from telegram.error import NetworkError
+import time
 
 
 def send_to_telegram(text, chat_id, token):
     # http://spys.one/proxys/US/
-    environ['HTTPS_PROXY'] = 'https://70.102.86.204:8080'
+    proxy_url = getenv('HTTPS_PROXY')
+    environ['HTTPS_PROXY'] = proxy_url
     bot = telegram.Bot(token=token)
     bot.send_message(chat_id=chat_id, text=text)
 
 
-def run_bot():
+def run_bot(
+        devman_token=None,
+        chat_id=None,
+        bot_token=None,
+        bad_message=None,
+        good_message=None
+):
+    api_url = 'https://dvmn.org/api/long_polling'
+    devman_url = 'https://dvmn.org/'
+    headers = {'Authorization': 'Token {}'.format(devman_token)}
+    params = {'timestamp': ''}
     while True:
         try:
             response = requests.get(api_url, headers=headers, params=params)
+            response.raise_for_status()
             json_data = response.json()
             if json_data['status'] == 'found':
                 for attempt in json_data['new_attempts']:
@@ -36,19 +50,17 @@ def run_bot():
                 params['timestamp'] = json_data['last_attempt_timestamp']
             if json_data['status'] == 'timeout':
                 params['timestamp'] = json_data['timestamp_to_request']
-        except (ConnectionError, ReadTimeout):
-            print('Timeout from Devman!')
+        except (ConnectionError, ReadTimeout, HTTPError) as error:
+            print(error)
+            time.sleep(5)
         except NetworkError as error:
             print('Error during send to Telegram :\n{}'.format(error))
+            time.sleep(5)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    api_url = 'https://dvmn.org/api/long_polling'
     devman_token = getenv('DEVMAN_TOKEN')
-    devman_url = 'https://dvmn.org/'
-    headers = {'Authorization': 'Token {}'.format(devman_token)}
-    params = {'timestamp': ''}
     chat_id = getenv('TELEGRAM_CHANNEL_NAME')
     bot_token = getenv('TELEGRAM_BOT_TOKEN')
     bad_message = 'У вас проверили работу "{}"\n' \
@@ -57,4 +69,10 @@ if __name__ == '__main__':
     good_message = 'У вас проверили работу "{}"\n' \
                    'Преподавателю все понравилось, ' \
                    'можно приступать к следующему уроку: {}modules!'
-    run_bot()
+    run_bot(
+        devman_token=devman_token,
+        chat_id=chat_id,
+        bot_token=bot_token,
+        bad_message=bad_message,
+        good_message=good_message
+    )
